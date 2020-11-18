6f464f44151c803d86c8355d81e2fb202c291d57
Ty <60036684+TyGillespie@users.noreply.github.com>
2020-11-18 15:33:18 -0700

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+__version__ = 0.1
+
+with open("readme.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="ag_py",
+    version=__version__,
+    author="Ty Gillespie",
+    author_email="turret1226@gmail.com",
+    description="A basic Audiogame Engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/tygillespie/.agpy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3",
+)
