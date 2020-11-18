59d51fd8cd0cc58f28f7d19245e9c81633dd808f
Ty <60036684+TyGillespie@users.noreply.github.com>
2020-11-18 08:33:55 -0700

    Added a few more utility functions.

--- agpy/utils.py
+++ agpy/utils.py
@@ -23,3 +23,19 @@ def is_admin() -> bool:
         return os.getuid() == 0
     except AttributeError:
         return ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+
+def percent(n1: int, n2: int) -> bool:
+    """Finds the percentage of n1 out of n2.
+    For example, if n1 was 5, and n2 was 10, it would return 50.
+    """
+    return (n1 / n2) * 100
+
+
+def generate_string(length: int) -> str:
+    """Generates a random string of a given lentgh."""
+    symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    final_string = ""
+    for i in range(length):
+        final_string += symbols[random.randint(0, len(symbols) - 1)]
+    return final_string
